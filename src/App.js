f21c21ca4befed616fd76a61ce74fffaca3b1858
Tom Ari <freak.tom3@gmail.com>
2022-03-19 18:30:49 +0200

    onChange for Inputs and FormDisplay

--- src/App.js
+++ src/App.js
@@ -1,14 +1,220 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
-import { Form } from "./components/Components";
+import { FormDisplay } from "./components/Components";
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      dob: "",
+      id: "",
+      email: "",
+      link1: "",
+      link2: "",
+      link3: "",
+      address: "",
+      zipCode: "",
+      phoneNumber: "",
+      title: "",
+      company: "",
+      location: "",
+      startDate: "",
+      endDate: "",
+      bullet: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App_forms">
-          <Form></Form>
+          <form>
+            <div className="forms_general">
+              <legend>General</legend>
+              <label htmlFor="firstName">first Name</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="lastName">last Name</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="email">email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="phoneNumber">Phone number</label>
+              <input
+                type="number"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={this.state.phoneNumber}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="DOB">date of birth</label>
+              <input
+                type="date"
+                id="DOB"
+                name="dob"
+                value={this.state.dob}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="address">address</label>
+              <input
+                type="address"
+                id="address"
+                name="address"
+                value={this.state.address}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="id">id</label>
+              <input
+                type="number"
+                id="id"
+                name="id"
+                value={this.state.id}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="link">link</label>
+              <input
+                type="url"
+                id="link"
+                placeholder="any relevant link goes here"
+                name="link1"
+                value={this.state.link1}
+                onChange={this.handleChange}
+              />
+
+              <button>add link</button>
+            </div>
+
+            <div className="forms_work">
+              <legend>Work Experience</legend>
+
+              <label htmlFor="title">title</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={this.state.title}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="company">company</label>
+              <input
+                type="text"
+                id="company"
+                name="company"
+                value={this.state.company}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="location">location</label>
+              <input
+                type="address"
+                id="location"
+                name="location"
+                value={this.state.location}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="startDate">start date</label>
+              <input
+                type="date"
+                id="startDate"
+                name="startDate"
+                value={this.state.startDate}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="endDate">end date</label>
+              <input
+                type="date"
+                id="endDate"
+                name="endDate"
+                value={this.state.endDate}
+                onChange={this.handleChange}
+              />
+
+              <label htmlFor="description">description</label>
+              <textarea
+                id="description"
+                placeholder="describe an aspect of your work in a bullet point"
+                name="bullet"
+                value={this.state.bullet}
+                onChange={this.handleChange}
+              ></textarea>
+
+              <button>add bullet</button>
+
+              <button>add work title</button>
+            </div>
+
+            <div className="forms_skills">
+              <legend>Miscellaneous Skills</legend>
+
+              <label htmlFor="category">category</label>
+              <input type="text" id="category" />
+
+              <label htmlFor="skills">skills</label>
+              <textarea
+                id="skills"
+                placeholder="add one at a time. Or, many, using, commas."
+              ></textarea>
+
+              <button>add skill</button>
+
+              <button>add category</button>
+            </div>
+          </form>
         </div>
+        <section className="form-display">
+          <FormDisplay
+            first={this.state.firstName}
+            last={this.state.lastName}
+            dob={this.state.dob}
+            id={this.state.id}
+            email={this.state.email}
+            link1={this.state.link1}
+            link2={this.state.link2}
+            link3={this.state.link3}
+            address={this.state.address}
+            zipCode={this.state.zipCode}
+            phoneNumber={this.state.phoneNumber}
+            title={this.state.title}
+            company={this.state.company}
+            location={this.state.location}
+            startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            bullet={this.state.bullet}
+          />
+        </section>
       </div>
     );
   }
--- src/components/Components.js
+++ src/components/Components.js
@@ -1,134 +1,58 @@
 import { Component } from "react";
 
-class General extends Component {
+class FormDisplay extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      general: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        phoneNumber: "",
-        dob: "",
-        address: "",
-        id: "",
-        links: [],
-      },
-    };
   }
-
   render() {
     return (
-      <div>
-        <legend>General</legend>
-        <label htmlFor="firstName">first Name</label>
-        <input type="text" id="firstName" />
-
-        <label htmlFor="lastName">last Name</label>
-        <input type="text" id="lastName" />
-
-        <label htmlFor="email">email</label>
-        <input type="email" id="email" />
-
-        <label htmlFor="phoneNumber">Phone number</label>
-        <input type="number" id="phoneNumber" />
-
-        <label htmlFor="DOB">date of birth</label>
-        <input type="date" id="DOB" />
-
-        <label htmlFor="address">address</label>
-        <input type="address" id="address" />
-
-        <label htmlFor="id">id</label>
-        <input type="number" id="id" />
-
-        <label htmlFor="link">link</label>
-        <input type="url" id="link" placeholder="any relevant link goes here" />
-
-        <button>add link</button>
-      </div>
+      <>
+        <div className="form-display_row">
+          <div className="form-display_row_header">
+            <h1>{this.props.first}</h1>
+            <h1>{this.props.last}</h1>
+          </div>
+          <div>
+            <p>DOB: {this.props.dob}</p>
+            <p>ID :{this.props.id}</p>
+          </div>
+          <div>
+            <p>{this.props.email}</p>
+            <p>{this.props.link1}</p>
+            <p>{this.props.link2}</p>
+            <p>{this.props.link3}</p>
+          </div>
+          <div>
+            <p>{this.props.address}</p>
+            <p>{this.props.zipCode}</p>
+            <p>{this.props.phoneNumber}</p>
+          </div>
+        </div>
+        <div className="form-display_row">
+          <div className="form-display_row_header">
+            <h2>work experience</h2>
+          </div>
+          <div className="form-display_row_work-details">
+            <div className="form-display_row_work-details_titles">
+              <span>{this.props.title}</span>
+              <span>{this.props.company}</span>
+              <span>{this.props.location}</span>
+            </div>
+            <div className="form-display_row_work-details_dates">
+              <span>{this.props.startDate}</span>
+              <span>-</span>
+              <span>{this.props.endDate}</span>
+            </div>
+          </div>
+          <div>
+            <ul>
+              <li>{this.props.bullet}</li>
+            </ul>
+          </div>
+        </div>
+      </>
     );
   }
 }
 
-class WorkExperience extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <legend>Work Experience</legend>
-
-        <label htmlFor="title">title</label>
-        <input type="text" id="title" />
-
-        <label htmlFor="location">location</label>
-        <input type="address" id="location" />
-
-        <label htmlFor="startDate">start date</label>
-        <input type="date" id="startDate" />
-
-        <label htmlFor="endDate">end date</label>
-        <input type="date" id="endDate" />
-
-        <label htmlFor="description">description</label>
-        <textarea
-          id="description"
-          placeholder="describe an aspect of your work in a bullet point"
-        ></textarea>
-
-        <button>add bullet</button>
-
-        <button>add work title</button>
-      </div>
-    );
-  }
-}
-
-class Skills extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <legend>Miscellaneous Skills</legend>
-
-        <label htmlFor="category">category</label>
-        <input type="text" id="category" />
-
-        <label htmlFor="skills">skills</label>
-        <textarea
-          id="skills"
-          placeholder="add one at a time. Or, many, using, commas."
-        ></textarea>
-
-        <button>add skill</button>
-
-        <button>add category</button>
-      </div>
-    );
-  }
-}
-
-class Form extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <form>
-        <General></General>
-        <WorkExperience></WorkExperience>
-        <Skills></Skills>
-      </form>
-    );
-  }
-}
-
-export { Form };
+export { FormDisplay };
